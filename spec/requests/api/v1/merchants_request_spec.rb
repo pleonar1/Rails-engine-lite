96329dcece68523ba37b4277218ec51b5a81f9b1
Paul Leonard <paulpaulleonard@gmail.com>
2022-03-29 23:22:55 -0400

    All the way to merchant #index

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Merchants Reuests", :type => :request do
   it "can get all merchants" do
     create_list(:merchant, 3)
 
-    get '/api/vi/merchants'
+    get '/api/v1/merchants'
 
     merchants = JSON.parse(response.body, symbolize_names: true)
     expect(response).to be_successful
