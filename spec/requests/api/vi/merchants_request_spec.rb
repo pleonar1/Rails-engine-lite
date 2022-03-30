555bac2bf874dbe0a95ada94a6e6baba122b2dd2
Paul Leonard <paulpaulleonard@gmail.com>
2022-03-29 22:42:17 -0400

    Add spec files for models

--- spec/requests/api/vi/merchants_request_spec.rb
+++ spec/requests/api/vi/merchants_request_spec.rb
@@ -3,7 +3,13 @@ require 'rails_helper'
 
 RSpec.describe "Merchants Reuests", :type => :request do
   it "can get all merchants" do
-    get '/merchants'
+    create_list(:merchant, 3)
+
+    get '/api/vi/merchants'
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_successful
+
 
   end
 
@@ -14,6 +20,4 @@ RSpec.describe "Merchants Reuests", :type => :request do
   it "can get all items for a given merchant id" do
 
   end
-
-
 end
