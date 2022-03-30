fe6bda7d814381e9387bd091a44c9409d4171383
Paul Leonard <paulpaulleonard@gmail.com>
2022-03-29 23:42:31 -0400

    Pass first test !

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -9,6 +9,14 @@ RSpec.describe "Merchants Reuests", :type => :request do
 
     merchants = JSON.parse(response.body, symbolize_names: true)
     expect(response).to be_successful
+    expect(merchants).to have_key(:data)
+    merchants[:data].each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant).to have_key(:type)
+      expect(merchant).to have_key(:attributes)
+      expect(merchant[:attributes]).to have_key(:name)
+    end
+    expect(merchants[:data].count).to eq(3)
   end
 
   it "can get one merchant" do
