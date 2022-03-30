b39622779d22d3e0c61ef680c365189b8324faf7
Paul Leonard <paulpaulleonard@gmail.com>
2022-03-29 22:56:18 -0400

    Add relations to all models

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do
+   it { should belong_to(:invoice) }
+   it { should belong_to(:item) }
+   it { should have_many(:bulk_discounts).through(:item) }
+  end
 end
