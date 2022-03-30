ee4411e48041e5af9a1eb1ff2e2145bfa37ebfce
Paul Leonard <paulpaulleonard@gmail.com>
2022-03-30 16:58:03 -0400

    Create item response test file and refactor Item factory

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :item do
-    
+    name { Faker::Company.name }
+    description { Faker::Address.city }
+    unit_price { Faker::Number.positive(from = 1.00, to = 5000.00) }
+    id { Faker::Number.unique.within(range: 1..100_000) }
   end
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,7 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "Items Reuests", :type => :request do
+  it "can get all items" do
+    merchant1 = create(:merchant)
+  	merchant2 = create(:merchant)
+  	create_list(:item, 2, merchant_id: merchant1.id)
+  	create_list(:item, 2, merchant_id: merchant2.id)
 
+    get "/api/v1/items"
 
+    items = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_successful
+  end
 
+  it "can get one item" do
+
+  end
+
+  it "can create an item" do
+
+  end
+
+  it "can edit an item" do
+
+  end
+
+  it "can delete an item" do
+
+  end
+
+  it "can get the merchant data for a gicen item id" do
+
+  end
 end
