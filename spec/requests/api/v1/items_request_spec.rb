1b8a999559a78e0e349975a330904cdd54bc3431
Paul Leonard <paulpaulleonard@gmail.com>
2022-03-31 16:27:23 -0400

    Finish Item#create

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -65,8 +65,10 @@ RSpec.describe "Items Requests", :type => :request do
     created_item = Item.last
 
     expect(response).to be_successful
-
-
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
   end
 
   it "can edit an item" do
